6bb5c32c1e63cf0c689036540193b7e20ad05283
mariosergiosl <mario.mssl@gmail.com>
2024-11-20 16:22:08 -0300

    Updating files: docs/conf.py docs/index.rst

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 
 project = 'memusage'
-copyright = '2024, Mario Luz'
+project_copyright = '2024, Mario Luz'
 author = 'Mario Luz'
 
 # The full version, including alpha/beta/rc tags
---
+++ docs/index.rst
@@ -0,0 +1,9 @@
+.. automodule:: memusage
+   :members:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
