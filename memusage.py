c2a75a6ea73f12abf6ea39229b0702bcd540fdb5
Mario Luz <mariosergiosl@users.noreply.github.com>
2025-10-03 16:59:50 -0300

    Update memusage.py

--- memusage.py
+++ memusage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2024 Mario Luz.
+# Copyright (C) 2024 Mario Luz
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
